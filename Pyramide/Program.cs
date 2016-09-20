7a6264fdf3f6a5f534880ca3a3845eafaae07d35
Xore <ITA1_16-Schaefer.JN@gmx.de>
2016-09-20 16:00:48 +0200

    Initial commit
    DAS GEHT SO NICHT

---
+++ Pyramide/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realMain();
+        }
+
+        static void realMain()
+        {
+            Console.Write("Select your methods! 1 or a = deineMutter(int) || 2 or b = deinVater(int)");
+            string answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "1":
+                case "A":
+                case "a":
+                    Console.Write("Give me numbers: ");
+                    try
+                    {
+                        int mHeight = Convert.ToInt32(Console.ReadLine());
+                        deineMutter(mHeight);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        realMain();
+                    }
+                    break;
+
+                case "2":
+                case "B":
+                case "b":
+                    Console.Write("Give me numbers: ");
+                    try
+                    {
+                        int vHeight = Convert.ToInt32(Console.ReadLine());
+                        deinVater(vHeight);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        realMain();
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("No method selected! Selecting default!\n"
+                                      + "Give me numbers: ");
+                    try
+                    {
+                        int dHeight = Convert.ToInt32(Console.ReadLine());
+                        deineMutter(dHeight);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        realMain();
+                    }
+
+                    break;
+            }
+        }
+        /*
+         Deklaration  || Bedingung || Danach
+         int ebene = 1 || Solange ebene kleiner oder gleich height || ebene + 1
+
+
+         Deklaration || Kombiniere(string,Wiederhole(string,anzahl));
+         Erzeuge string stern = kombiniere mehrere Strings("Whitespace", Wiederhole("*", ebene));
+         Do(string.PadLeft(number);
+
+         Wir ziehen von den Reihen die wir erstellen sollen, die gerade erstelle ab und fügen die Länge vom string stern hinzu
+         Number = height - ebene + string.Länge 
+
+
+         Schreibe(stern.Rechtsbündig(height - ebene + stern.Länge));
+        */
+        static void deineMutter(int height)
+        {
+            for (int ebene = 1; ebene <= height; ebene++)
+            {
+                string stern = string.Join(" ", Enumerable.Repeat("*", ebene));
+                Console.WriteLine(stern.PadLeft(height - ebene + stern.Length));
+            }
+            realMain();
+        }
+        /*
+         count = 1
+         solange(lines = height; lines größer oder gleich 1; lines -1)
+            solange(whitespaces = lines -1; whitespaces kleiner oder grö0er 1; whitespaces -1)
+                Schreib("Whitespace");
+            solange(stern = 1; stern kleiner oder gleich count; stern +1)
+                Schreib("*");
+                schreib("Whitespace");
+            count + 1
+            Schreib(Neue Zeile);
+         */
+        static void deinVater(int height)
+        {
+            int count = 1;
+            for (int lines = height; lines >= 1; lines--)
+            {
+                for (int whitespaces = lines - 1; whitespaces >= 1; whitespaces--)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int stern = 1; stern <= count; stern++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+
+                }
+                count++;
+                Console.Write(Environment.NewLine);
+            }
+            realMain();
+        }
+    }
+}
---
+++ Welle/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            realMain();
+        }
+
+        static void realMain()
+        {
+            float d1, d2, l1, l2, r1, r2, vol1, vol2, volall, mass1, mass2, massall, dichte;
+            dichte = 7.8f;
+
+            Console.Write("\tPlease enter d1: ");
+            d1 = Convert.ToSingle(Console.ReadLine());
+            Console.Write("\tPlease enter l1: ");
+            l1 = Convert.ToSingle(Console.ReadLine());
+
+            r1 = d1 / 2;
+            vol1 = Convert.ToSingle(Math.PI * Math.Pow(r1, 2) * l1);
+            mass1 = dichte * vol1;
+
+            Console.Write("\tPlease enter d2: ");
+            d2 = Convert.ToSingle(Console.ReadLine());
+            Console.Write("\tPlease enter l2: ");
+            l2 = Convert.ToSingle(Console.ReadLine());
+
+            r2 = d2 / 2;
+            vol2 = Convert.ToSingle(Math.PI * Math.Pow(r2, 2) * l2);
+            mass2 = dichte * vol2;
+
+            volall = (vol2 * 2) + vol1;
+            massall = (mass2 * 2) + mass1;
+
+            Console.Write("\tDas Gesamtvolumen beträgt {0}mm³\n"
+                           + "\tDie Gesamtmasse beträgt {1}kg\n"
+                           + "\tWiederholen? (Y)es (N)o\n\t", volall, massall);
+
+            string answer = Console.ReadLine();
+            if(answer.ToLower().FirstOrDefault() == 'y')
+            {
+                realMain();
+            }
+        }
+    }
+}
